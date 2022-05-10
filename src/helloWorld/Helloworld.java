2bcbc450cc7d46fe931d5b6826244f08a55d6e85
duswn <duswn@192.168.123.114>
2022-05-10 17:44:41 +0900

    token

--- src/helloWorld/Helloworld.java
+++ src/helloWorld/Helloworld.java
@@ -3,7 +3,7 @@ package helloWorld;
 public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("hi");
+		System.out.println("hi ");
 		
 	} 
 }
